6f837fa556ad6751ff398ddcd88f48da81a8611d
Michal <galusmichal@outlook.com>
2025-12-28 10:32:40 +0100

    SCRUM-01 added Image.java

---
+++ src/main/java/pl/strefainformacji/entity/Image.java
@@ -0,0 +1,17 @@
+package pl.strefainformacji.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "images")
+public class Image {
+}
