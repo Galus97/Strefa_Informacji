66626a831d2cad83d3ec9cc9ec48a5e6cc54b4f4
Michal <galusmichal@outlook.com>
2026-01-03 15:43:26 +0100

    SCRUM-01 added Annotation & repo to ImageService

--- src/main/java/pl/strefainformacji/service/ImageService.java
+++ src/main/java/pl/strefainformacji/service/ImageService.java
@@ -1,4 +1,11 @@
  package pl.strefainformacji.service;
 
+ import lombok.RequiredArgsConstructor;
+ import org.springframework.stereotype.Service;
+ import pl.strefainformacji.repository.ImageRepository;
+
+ @Service
+ @RequiredArgsConstructor
 public class ImageService {
+     private final ImageRepository imageRepository;
 }
