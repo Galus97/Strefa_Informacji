37ad8d473f223f04108941c691152dbe10223848
Michal <galusmichal@outlook.com>
2026-01-04 17:56:30 +0100

    SCRUM-01 added getMessage to MessageService

--- src/main/java/pl/strefainformacji/component/MessageService.java
+++ src/main/java/pl/strefainformacji/component/MessageService.java
@@ -1,4 +1,20 @@
 package pl.strefainformacji.component;
 
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+@RequiredArgsConstructor
 public class MessageService {
+    private final MessageSource messageSource;
+
+    public String getMessage(String key, Object... params) {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage(key, params, locale);
+    }
 }
