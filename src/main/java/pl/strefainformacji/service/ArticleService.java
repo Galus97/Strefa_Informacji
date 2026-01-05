a27c2c8d16c704e4661724d514a29531659a1c13
Michal <galusmichal@outlook.com>
2026-01-06 00:01:09 +0100

    SCRUM-01 added getArticleOrThrowIfNotExist to ArticleService

--- src/main/java/pl/strefainformacji/service/ArticleService.java
+++ src/main/java/pl/strefainformacji/service/ArticleService.java
@@ -2,16 +2,24 @@ package pl.strefainformacji.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.strefainformacji.component.MessageService;
+import pl.strefainformacji.entity.Article;
 import pl.strefainformacji.repository.ArticleRepository;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleRepository articleRepository;
+    private final MessageService messageService;
 
-    public void throwIfIdIsInvalid (Long id, String message) {
+    private void throwIfIdIsInvalid (Long id, String message) {
         if(id == null || id < 0) {
             throw new IllegalArgumentException(message);
         }
+    }
+
+    private Article getArticleOrThrowIfNotExist (Long id, String message) {
+        return articleRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException(messageService.getMessage(message)));
     }
 }
