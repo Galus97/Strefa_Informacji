e0303b183955cc8250be16bc85f5db4cf158c299
Michal <galusmichal@outlook.com>
2026-01-08 19:17:44 +0100

    SCRUM-01 added fromEntity to ArticleResponse

--- src/main/java/pl/strefainformacji/dto/response/ArticleResponse.java
+++ src/main/java/pl/strefainformacji/dto/response/ArticleResponse.java
@@ -1,5 +1,14 @@
 package pl.strefainformacji.dto.response;
 
+import pl.strefainformacji.entity.Article;
+
 public record ArticleResponse(Long articleId, String title, String shortDescription, String description) {
 
+    public static ArticleResponse fromEntity(Article article) {
+        return new ArticleResponse(
+                article.getArticleId(),
+                article.getTitle(),
+                article.getShortDescription(),
+                article.getDescription());
+    }
 }
