b050305eabe70fa8ebc5bbe4d24c922cfd68467b
Michal <galusmichal@outlook.com>
2026-01-12 21:52:50 +0100

    SCRUM-01 added getArticle to ArticleController

--- src/main/java/pl/strefainformacji/controller/ArticleController.java
+++ src/main/java/pl/strefainformacji/controller/ArticleController.java
@@ -1,11 +1,22 @@
 package pl.strefainformacji.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.strefainformacji.dto.response.ArticleResponse;
+import pl.strefainformacji.service.ArticleService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/article")
 public class ArticleController {
+    private final ArticleService articleService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ArticleResponse> getArticle(@PathVariable Long id) {
+        return ResponseEntity.ok(articleService.getArticleResponse(id));
+    }
 }
