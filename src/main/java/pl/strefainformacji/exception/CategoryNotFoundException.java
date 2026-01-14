8a6a2f51f53eb946badaa22fb93fc1da1118c316
Michal <galusmichal@outlook.com>
2026-01-14 21:27:52 +0100

    SCRUM-01 deleted CategoryNotFoundException.java

--- src/main/java/pl/strefainformacji/exception/CategoryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package pl.strefainformacji.exception;
-
-public class CategoryNotFoundException extends RuntimeException {
-    public CategoryNotFoundException (String message) {
-        super(message);
-    }
-}
