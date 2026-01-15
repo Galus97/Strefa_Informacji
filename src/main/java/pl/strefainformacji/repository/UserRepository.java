805e6d96468a77170f0710800b29b04ea6f571af
Michal <galusmichal@outlook.com>
2026-01-15 09:41:03 +0100

    SCRUM-01 added findByEmail to UserRepository

--- src/main/java/pl/strefainformacji/repository/UserRepository.java
+++ src/main/java/pl/strefainformacji/repository/UserRepository.java
@@ -3,5 +3,8 @@ package pl.strefainformacji.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.strefainformacji.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
