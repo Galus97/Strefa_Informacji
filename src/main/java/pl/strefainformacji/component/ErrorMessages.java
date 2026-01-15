839c3013520148e1d2fcddece0322e5678fbb22c
Michal <galusmichal@outlook.com>
2026-01-15 09:46:33 +0100

    SCRUM-01 added USER_NOT_FOUND to ErrorMessages

--- src/main/java/pl/strefainformacji/component/ErrorMessages.java
+++ src/main/java/pl/strefainformacji/component/ErrorMessages.java
@@ -6,4 +6,6 @@ public final class ErrorMessages {
     public static final String ARTICLE_REQUEST_IS_NULL = "error.articleRequestIsNull";
 
     public static final String EMAIL_IS_INVALID = "error.invalidEmail";
+
+    public static final String USER_NOT_FOUND =  "error.userNotFound";
 }
