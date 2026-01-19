3868d40be84de323ee92a862066144b4f2b38070
Michal <galusmichal@outlook.com>
2026-01-19 07:54:35 +0100

    SCRUM-01 reformated code

--- src/main/java/pl/strefainformacji/StrefaInformacjiApplication.java
+++ src/main/java/pl/strefainformacji/StrefaInformacjiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StrefaInformacjiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StrefaInformacjiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StrefaInformacjiApplication.class, args);
+    }
 
 }
--- src/main/java/pl/strefainformacji/service/ImageService.java
+++ src/main/java/pl/strefainformacji/service/ImageService.java
@@ -1,33 +1,33 @@
- package pl.strefainformacji.service;
+package pl.strefainformacji.service;
 
- import lombok.RequiredArgsConstructor;
- import org.springframework.stereotype.Service;
- import pl.strefainformacji.component.ErrorMessages;
- import pl.strefainformacji.component.MessageService;
- import pl.strefainformacji.dto.response.ImageResponse;
- import pl.strefainformacji.entity.Image;
- import pl.strefainformacji.exception.ImageNotFoundException;
- import pl.strefainformacji.repository.ImageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.strefainformacji.component.ErrorMessages;
+import pl.strefainformacji.component.MessageService;
+import pl.strefainformacji.dto.response.ImageResponse;
+import pl.strefainformacji.entity.Image;
+import pl.strefainformacji.exception.ImageNotFoundException;
+import pl.strefainformacji.repository.ImageRepository;
 
- @Service
- @RequiredArgsConstructor
+@Service
+@RequiredArgsConstructor
 public class ImageService {
-     private final ImageRepository imageRepository;
-     private final MessageService messageService;
+    private final ImageRepository imageRepository;
+    private final MessageService messageService;
 
-     public ImageResponse getImageResponse (Long imageId) {
+    public ImageResponse getImageResponse(Long imageId) {
         throwIfIdIsInvalid(imageId);
         return ImageResponse.fromEntity(getImageOrThrowIfNotExist(imageId));
-     }
+    }
 
-     private void throwIfIdIsInvalid(Long imageId) {
-      if(imageId == null || imageId <= 0) {
-         throw new IllegalArgumentException(messageService.getMessage(ErrorMessages.INVALID_IMAGE_ID));
+    private void throwIfIdIsInvalid(Long imageId) {
+        if (imageId == null || imageId <= 0) {
+            throw new IllegalArgumentException(messageService.getMessage(ErrorMessages.INVALID_IMAGE_ID));
         }
-     }
+    }
 
-     private Image getImageOrThrowIfNotExist(Long imageId) {
+    private Image getImageOrThrowIfNotExist(Long imageId) {
         return imageRepository.findById(imageId).orElseThrow(
-              () -> new ImageNotFoundException(messageService.getMessage(ErrorMessages.IMAGE_NOT_FOUND, imageId)));
-     }
+                () -> new ImageNotFoundException(messageService.getMessage(ErrorMessages.IMAGE_NOT_FOUND, imageId)));
+    }
 }
