7e168428ea8039e94fb40746f0828e802300e1b0
Michal <galusmichal@outlook.com>
2026-01-19 07:45:23 +0100

    SCRUM-01 added getImageOrThrowIfNotExist to ImageService

--- src/main/java/pl/strefainformacji/service/ImageService.java
+++ src/main/java/pl/strefainformacji/service/ImageService.java
@@ -4,6 +4,9 @@
  import org.springframework.stereotype.Service;
  import pl.strefainformacji.component.ErrorMessages;
  import pl.strefainformacji.component.MessageService;
+ import pl.strefainformacji.dto.response.ImageResponse;
+ import pl.strefainformacji.entity.Image;
+ import pl.strefainformacji.exception.ImageNotFoundException;
  import pl.strefainformacji.repository.ImageRepository;
 
  @Service
@@ -12,9 +15,15 @@ public class ImageService {
      private final ImageRepository imageRepository;
      private final MessageService messageService;
 
+
      private void throwIfIdIsInvalid(Long imageId) {
       if(imageId == null || imageId <= 0) {
-        throw new IllegalArgumentException(messageService.getMessage(ErrorMessages.INVALID_IMAGE_ID));
-      }
+         throw new IllegalArgumentException(messageService.getMessage(ErrorMessages.INVALID_IMAGE_ID));
+        }
+     }
+
+     private Image getImageOrThrowIfNotExist(Long imageId) {
+        return imageRepository.findById(imageId).orElseThrow(
+              () -> new ImageNotFoundException(messageService.getMessage(ErrorMessages.IMAGE_NOT_FOUND, imageId)));
      }
 }
