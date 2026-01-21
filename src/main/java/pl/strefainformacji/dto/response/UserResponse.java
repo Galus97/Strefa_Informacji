f32cc619e13dcfe3078060572894789d48d6751e
Michal <galusmichal@outlook.com>
2026-01-21 19:53:31 +0100

    SCRUM-01 added fromEntity to UserResponse

--- src/main/java/pl/strefainformacji/dto/response/UserResponse.java
+++ src/main/java/pl/strefainformacji/dto/response/UserResponse.java
@@ -1,4 +1,18 @@
 package pl.strefainformacji.dto.response;
 
-public record UserResponse(Long userId, String firstName, String lastName, String email,boolean enabled, String emailCode) {
+import pl.strefainformacji.entity.User;
+
+public record UserResponse(Long userId, String firstName, String lastName, String email,
+                           boolean enabled, String emailCode) {
+
+    public static UserResponse fromEntity(User user) {
+        return new UserResponse(
+                user.getUserId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.isEnabled(),
+                user.getEmailCode()
+        );
+    }
 }
