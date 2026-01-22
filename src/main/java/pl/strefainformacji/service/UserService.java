6ec52894f0812a1bfac9e3f10d6de18b315a91ce
Michal <galusmichal@outlook.com>
2026-01-23 00:41:07 +0100

    SCRUM-01 added updateUser to UserService

--- src/main/java/pl/strefainformacji/service/UserService.java
+++ src/main/java/pl/strefainformacji/service/UserService.java
@@ -1,6 +1,7 @@
 package pl.strefainformacji.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.strefainformacji.component.ErrorMessages;
@@ -16,6 +17,7 @@ import pl.strefainformacji.repository.UserRepository;
 public class UserService {
     private final UserRepository userRepository;
     private final MessageService messageService;
+    private final PasswordEncoder passwordEncoder;
 
     public UserResponse getUserResponse(Long userId){
         throwIfIdIsInvalid(userId);
@@ -27,6 +29,22 @@ public class UserService {
         userRepository.deleteById(userId);
     }
 
+    @Transactional
+    public UserResponse updateUser(UserRequest userRequest){
+        throwIfRequestIsNull(userRequest);
+        throwIfIdIsInvalid(userRequest.getUserId());
+
+        User existingUser = getUserOrThrowIfNotExist(userRequest.getUserId());
+        existingUser.setFirstName(userRequest.getFirstName());
+        existingUser.setLastName(userRequest.getLastName());
+        existingUser.setEmail(userRequest.getEmail());
+
+        if (userRequest.getPassword() != null && !userRequest.getPassword().isEmpty()) {
+            existingUser.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+        }
+        return UserResponse.fromEntity(userRepository.save(existingUser));
+
+    }
 
     private void throwIfIdIsInvalid(Long id) {
         if(id == null || id <= 0) {
