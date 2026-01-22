1843474275201302f79f120152057c56b2f0f5f1
Michal <galusmichal@outlook.com>
2026-01-22 16:57:08 +0100

    SCRUM-01 added getUserResponse to UserService

--- src/main/java/pl/strefainformacji/service/UserService.java
+++ src/main/java/pl/strefainformacji/service/UserService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.strefainformacji.component.ErrorMessages;
 import pl.strefainformacji.component.MessageService;
+import pl.strefainformacji.dto.response.UserResponse;
 import pl.strefainformacji.entity.User;
 import pl.strefainformacji.exception.UserNotFoundException;
 import pl.strefainformacji.repository.UserRepository;
@@ -13,6 +14,11 @@ import pl.strefainformacji.repository.UserRepository;
 public class UserService {
     private final UserRepository userRepository;
     private final MessageService messageService;
+
+    public UserResponse getUserResponse(Long userId){
+        throwIfIdIsInvalid(userId);
+        return UserResponse.fromEntity(getUserOrThrowIfNotExist(userId));
+    }
 
     private void throwIfIdIsInvalid(Long id) {
         if(id == null || id <= 0) {
