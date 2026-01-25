55e3247ce57081fac9c9b1e930d43d4b3a9cfa17
Michal <galusmichal@outlook.com>
2026-01-25 22:46:46 +0100

    SCRUM-01 added SpringSecurity.java

---
+++ src/main/java/pl/strefainformacji/conifiguration/SpringSecurity.java
@@ -0,0 +1,9 @@
+package pl.strefainformacji.conifiguration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity {
+}
