be52bf87b1bb67b3203f1d210b93181e6a3ec74b
Michal <galusmichal@outlook.com>
2026-01-31 08:23:14 +0100

    SCRUM-01 added getMapResponseEntity to GlobalExceptionHandler

--- src/main/java/pl/strefainformacji/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/strefainformacji/exception/GlobalExceptionHandler.java
@@ -1,7 +1,19 @@
 package pl.strefainformacji.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pl.strefainformacji.component.ErrorMessages;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static ResponseEntity<Map<String, String>> getMapResponseEntity(RuntimeException e) {
+        Map<String, String> response = new HashMap<>();
+        response.put(ErrorMessages.ERROR, e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
 }
