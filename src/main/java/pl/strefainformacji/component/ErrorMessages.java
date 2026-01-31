5868d7c2444b0d31d425cb536bcf6501a742a6a6
Michal <galusmichal@outlook.com>
2026-01-31 08:23:25 +0100

    SCRUM-01 added ERROR to ErrorMessages

--- src/main/java/pl/strefainformacji/component/ErrorMessages.java
+++ src/main/java/pl/strefainformacji/component/ErrorMessages.java
@@ -1,6 +1,8 @@
 package pl.strefainformacji.component;
 
 public final class ErrorMessages {
+    public static final String ERROR = "error";
+
     public static final String ARTICLE_NOT_FOUND = "error.articleNotFound";
     public static final String INVALID_ARTICLE_ID = "error.invalidArticleId";
     public static final String ARTICLE_REQUEST_IS_NULL = "error.articleRequestIsNull";
