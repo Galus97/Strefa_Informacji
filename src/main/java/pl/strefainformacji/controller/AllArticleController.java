ab9e88cd5bd6f1c66cc06080d9f77b44b41204b9
Michal <galusmichal@outlook.com>
2026-02-05 21:46:21 +0100

    SCRUM-01 added AllArticleController.java

---
+++ src/main/java/pl/strefainformacji/controller/AllArticleController.java
@@ -0,0 +1,11 @@
+package pl.strefainformacji.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/all")
+public class AllArticleController {
+}
