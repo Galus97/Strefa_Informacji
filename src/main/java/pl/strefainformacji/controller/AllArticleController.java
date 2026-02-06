67c0a1cedc84ada060c86409b0813081e7fbf94c
Michal <galusmichal@outlook.com>
2026-02-06 21:52:27 +0100

    SCRUM-01 added getAllArticles to AllArticleController

--- src/main/java/pl/strefainformacji/controller/AllArticleController.java
+++ src/main/java/pl/strefainformacji/controller/AllArticleController.java
@@ -1,11 +1,21 @@
 package pl.strefainformacji.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.strefainformacji.dto.response.ArticleResponse;
+import pl.strefainformacji.service.ArticleService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/all")
 public class AllArticleController {
+    private final ArticleService articleService;
+
+    public ResponseEntity<List<ArticleResponse>> getAllArticles() {
+        return ResponseEntity.ok(articleService.getAllArticles());
+    }
 }
