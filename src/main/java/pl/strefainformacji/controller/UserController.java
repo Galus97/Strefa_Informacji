81d9eddd72c32d5051f260b204c8c4a70fd765a7
Michal <galusmichal@outlook.com>
2026-02-08 11:29:45 +0100

    SCRUM-01 added updateUser to UserController

--- src/main/java/pl/strefainformacji/controller/UserController.java
+++ src/main/java/pl/strefainformacji/controller/UserController.java
@@ -4,9 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.strefainformacji.dto.request.UserRequest;
 import pl.strefainformacji.dto.response.UserResponse;
+import pl.strefainformacji.exception.ValidationException;
 import pl.strefainformacji.service.UserService;
 
 @RestController
@@ -18,5 +23,19 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> showUser(@PathVariable Long id) {
         return ResponseEntity.ok(userService.getUserResponse(id));
+    }
+
+//    @PostMapping
+//    public ResponseEntity<?> saveUser(@RequestBody UserRequest userRequest) {
+//        try {
+//
+//        } catch (ValidationException e) {
+//            return ResponseEntity.badRequest().body(e.getValidationsErrors());
+//        }
+//    }
+
+    @PutMapping
+    public ResponseEntity<UserResponse> updateUser(@RequestBody UserRequest userRequest) {
+        return ResponseEntity.ok(userService.updateUser(userRequest));
     }
 }
