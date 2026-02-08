f1bf5653cf1e2f18d01c4e6cebf985393835081d
Michal <galusmichal@outlook.com>
2026-02-08 11:18:47 +0100

    SCRUM-01 added showUser to UserController

--- src/main/java/pl/strefainformacji/controller/UserController.java
+++ src/main/java/pl/strefainformacji/controller/UserController.java
@@ -1,11 +1,22 @@
 package pl.strefainformacji.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.strefainformacji.dto.response.UserResponse;
+import pl.strefainformacji.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> showUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserResponse(id));
+    }
 }
