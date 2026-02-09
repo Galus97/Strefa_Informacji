fad694345e1a31fd5b74f76b75b54260d27fc7fb
Michal <galusmichal@outlook.com>
2026-02-09 21:51:15 +0100

    SCRUM-01 added deleteUser to UserController

--- src/main/java/pl/strefainformacji/controller/UserController.java
+++ src/main/java/pl/strefainformacji/controller/UserController.java
@@ -2,6 +2,7 @@ package pl.strefainformacji.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,5 +38,11 @@ public class UserController {
     @PutMapping
     public ResponseEntity<UserResponse> updateUser(@RequestBody UserRequest userRequest) {
         return ResponseEntity.ok(userService.updateUser(userRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
