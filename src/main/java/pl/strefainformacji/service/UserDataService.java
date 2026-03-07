929b613d99c1ea4aaf612a2d35901a89557a4472
Michal <galusmichal@outlook.com>
2026-03-07 22:41:33 +0100

    SCRUM-01 added throwIfIdIsInvalid to UserDataService

--- src/main/java/pl/strefainformacji/service/UserDataService.java
+++ src/main/java/pl/strefainformacji/service/UserDataService.java
@@ -3,10 +3,18 @@ package pl.strefainformacji.service;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import pl.strefainformacji.component.ErrorMessages;
+import pl.strefainformacji.component.MessageService;
 import pl.strefainformacji.repository.UserDataRepository;
 
 @Service
 @RequiredArgsConstructor
 public class UserDataService {
     private final UserDataRepository userDataRepository;
+    private final MessageService messageService;
+
+    private void throwIfIdIsInvalid(Long id) {
+        if(id == null || id < 1) {
+            throw new IllegalArgumentException(messageService.getMessage(ErrorMessages.INVALID_USER_DATA_ID));        }
+    }
 }
