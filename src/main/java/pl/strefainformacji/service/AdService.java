e04b317364bc58c13769d669cbf23d387d5b90d8
Michal <galusmichal@outlook.com>
2026-04-02 22:16:05 +0200

    SCRUM-01 added saveAd to  AdService

--- src/main/java/pl/strefainformacji/service/AdService.java
+++ src/main/java/pl/strefainformacji/service/AdService.java
@@ -1,5 +1,16 @@
 package pl.strefainformacji.service;
 
-public class AdService {
+import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
+import pl.strefainformacji.repository.AdRepository;
+
+@Service
+@AllArgsConstructor
+public class AdService {
+    private final AdRepository adRepository;
+
+    public void saveAdd() {
+        
+    }
 }
