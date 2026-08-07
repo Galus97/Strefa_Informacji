2abc4ed1c56d641273cb475a44ae08714f8352c9
michalgalus <galusmichal@outlook.com>
2026-08-07 23:16:25 +0200

    SCRUM-01 added throwIfRequestIsNull to ServiceValidator

--- src/main/java/pl/strefainformacji/util/ServiceValidator.java
+++ src/main/java/pl/strefainformacji/util/ServiceValidator.java
@@ -10,8 +10,14 @@ public class ServiceValidator {
     private final MessageService messageService;
 
     public void throwIfIdIsNotValid(Long id, String errorMessage) {
-        if (id != null && id <= 0) {
+        if (id == null || id <= 0) {
             throw new IllegalArgumentException(messageService.getMessage(errorMessage, id));
+        }
+    }
+
+    public void throwIfRequestIsNull(Object request, String errorMessage) {
+        if (request == null) {
+            throw new IllegalArgumentException(messageService.getMessage(errorMessage));
         }
     }
 }
