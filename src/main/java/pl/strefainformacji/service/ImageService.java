31a854cb7bde911487388a9f8f1de9b9669ca8b8
michalgalus <galusmichal@outlook.com>
2026-08-07 23:37:17 +0200

    SCRUM-01 added ServiceValidator to ImageService

--- src/main/java/pl/strefainformacji/service/ImageService.java
+++ src/main/java/pl/strefainformacji/service/ImageService.java
@@ -12,53 +12,42 @@ import pl.strefainformacji.exception.ArticleNotFoundException;
 import pl.strefainformacji.exception.ImageNotFoundException;
 import pl.strefainformacji.repository.ArticleRepository;
 import pl.strefainformacji.repository.ImageRepository;
+import pl.strefainformacji.util.ServiceValidator;
 
 @Service
 @RequiredArgsConstructor
 public class ImageService {
     private final ImageRepository imageRepository;
     private final MessageService messageService;
-    private final ArticleRepository articleRepository;
-    
-
-    public ImageResponse saveImageResponse(ImageRequest imageRequest) {
-        throwIfRequestIsNotValid(imageRequest);
-        return ImageResponse.fromEntity(imageRepository.save(buildImageFromRequest(imageRequest)));
-    }
+    private final ArticleService articleService;
+    private final ServiceValidator serviceValidator;
 
     public ImageResponse getImageResponse(Long imageId) {
-        throwIfIdIsInvalid(imageId);
+        serviceValidator.throwIfIdIsNotValid(imageId, ErrorMessages.INVALID_IMAGE_ID);
         return ImageResponse.fromEntity(getImageOrThrowIfNotExist(imageId));
     }
 
+    public ImageResponse saveImage(ImageRequest imageRequest) {
+        serviceValidator.throwIfRequestIsNull(imageRequest, ErrorMessages.IMAGE_REQUEST_IS_NULL);
+        return ImageResponse.fromEntity(imageRepository.save(buildImageFromRequest(imageRequest)));
+    }
+
     public void deleteImage(Long imageId) {
-        throwIfIdIsInvalid(imageId);
+        serviceValidator.throwIfIdIsNotValid(imageId, ErrorMessages.INVALID_IMAGE_ID);
         imageRepository.delete(getImageOrThrowIfNotExist(imageId));
     }
 
-    private void throwIfIdIsInvalid(Long imageId) {
-        if (imageId == null || imageId <= 0) {
-            throw new IllegalArgumentException(messageService.getMessage(ErrorMessages.INVALID_IMAGE_ID));
-        }
-    }
-
-    public void throwIfRequestIsNotValid(ImageRequest imageRequest) {
-        if(imageRequest == null) {
-            throw new NullPointerException(messageService.getMessage(ErrorMessages.IMAGE_REQUEST_IS_NULL));
-        }
-    }
-
     private Image getImageOrThrowIfNotExist(Long imageId) {
-        return imageRepository.findById(imageId).orElseThrow(
-                () -> new ImageNotFoundException(messageService.getMessage(ErrorMessages.IMAGE_NOT_FOUND, imageId)));
+        return imageRepository.findById(imageId)
+                .orElseThrow(() -> new ImageNotFoundException(
+                        messageService.getMessage(ErrorMessages.IMAGE_NOT_FOUND, imageId)));
     }
 
     private Image buildImageFromRequest(ImageRequest imageRequest) {
         return Image.builder()
                 .srcImg(imageRequest.getSrcImg())
                 .altImg(imageRequest.getAltImg())
-                .article(articleRepository.findById(imageRequest.getArticleRequest().getArticleId())
-                        .orElseThrow(() -> new ArticleNotFoundException(messageService.getMessage(ErrorMessages.ARTICLE_NOT_FOUND))))
+                .article(articleService.getArticleOrThrowIfNotExist(imageRequest.getArticleId()))
                 .build();
     }
 }
