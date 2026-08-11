b51efa5aedd8f1a980161595017f97fc2c7d605f
michalgalus <galusmichal@outlook.com>
2026-08-11 20:45:32 +0200

    SCRUM-01 added showUserData to UserDataController

--- src/main/java/pl/strefainformacji/controller/UserDataController.java
+++ src/main/java/pl/strefainformacji/controller/UserDataController.java
@@ -1,11 +1,22 @@
 package pl.strefainformacji.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.strefainformacji.dto.response.UserDataResponse;
+import pl.strefainformacji.service.UserDataService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/userdata")
 public class UserDataController {
+    private final UserDataService userDataService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDataResponse> showUserData(@PathVariable Long id) {
+        return ResponseEntity.ok(userDataService.getUserData(id));
+    }
 }
