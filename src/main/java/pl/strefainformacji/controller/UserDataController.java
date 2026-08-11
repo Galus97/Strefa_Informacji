7dc2fc7411b244efd21291fb2daa323dc85cd72c
michalgalus <galusmichal@outlook.com>
2026-08-11 20:51:52 +0200

    SCRUM-01 added saveUserData to UserDataController

--- src/main/java/pl/strefainformacji/controller/UserDataController.java
+++ src/main/java/pl/strefainformacji/controller/UserDataController.java
@@ -2,12 +2,12 @@ package pl.strefainformacji.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.strefainformacji.dto.request.UserDataRequest;
 import pl.strefainformacji.dto.response.UserDataResponse;
 import pl.strefainformacji.service.UserDataService;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +18,12 @@ public class UserDataController {
     @GetMapping("/{id}")
     public ResponseEntity<UserDataResponse> showUserData(@PathVariable Long id) {
         return ResponseEntity.ok(userDataService.getUserData(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDataResponse> saveUserData(@RequestBody UserDataRequest userDataRequest) {
+        UserDataResponse savedUserData = userDataService.saveUserData(userDataRequest);
+        return ResponseEntity.created(URI.create("/userdata/" + savedUserData.UserDataId()))
+                .body(savedUserData);
     }
 }
