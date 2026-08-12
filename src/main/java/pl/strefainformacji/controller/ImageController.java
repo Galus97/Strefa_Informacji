609886cf543e0a89c8860380462e8ae1408eb0e2
michalgalus <galusmichal@outlook.com>
2026-08-12 06:26:08 +0200

    SCRUM-01 added showImage to ImageController

--- src/main/java/pl/strefainformacji/controller/ImageController.java
+++ src/main/java/pl/strefainformacji/controller/ImageController.java
@@ -1,11 +1,24 @@
 package pl.strefainformacji.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.strefainformacji.dto.response.ImageResponse;
+import pl.strefainformacji.service.ImageService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/image")
 public class ImageController {
+    private final ImageService imageService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ImageResponse> showImage(@PathVariable Long id) {
+        return ResponseEntity.ok(imageService.getImage(id));
+    }
+
+
 }
