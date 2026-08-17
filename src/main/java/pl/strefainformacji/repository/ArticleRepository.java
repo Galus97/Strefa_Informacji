b8e23ccf26a421e888b369c06db872a690327cbf
michalgalus <galusmichal@outlook.com>
2026-08-17 21:09:30 +0200

    SCRUM-01 added findAllByCreatedAtBetween to ArticleRepository

--- src/main/java/pl/strefainformacji/repository/ArticleRepository.java
+++ src/main/java/pl/strefainformacji/repository/ArticleRepository.java
@@ -1,6 +1,8 @@
 package pl.strefainformacji.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.strefainformacji.component.Category;
 import pl.strefainformacji.entity.Article;
 import pl.strefainformacji.component.Tag;
@@ -14,5 +16,8 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     List<Article> findArticlesByTags(List<Tag> tags);
 
-    List<Article> findAllByCreatedAtBetween(LocalDateTime from, LocalDateTime to);
+    @Query("SELECT a FROM Article a WHERE a.createdAt BETWEEN :from AND :to")
+    List<Article> findAllByCreatedAtBetween(
+            @Param("from")LocalDateTime from,
+            @Param("to") LocalDateTime to);
 }
