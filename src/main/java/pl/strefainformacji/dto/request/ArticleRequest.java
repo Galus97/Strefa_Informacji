ef6faae65abb84d2d71698eb0e048ad83dfb5fbd
michalgalus <galusmichal@outlook.com>
2026-08-21 05:40:45 +0200

    SCRUM-01 updated ArticleRequest

--- src/main/java/pl/strefainformacji/dto/request/ArticleRequest.java
+++ src/main/java/pl/strefainformacji/dto/request/ArticleRequest.java
@@ -2,16 +2,19 @@ package pl.strefainformacji.dto.request;
 
 import lombok.Builder;
 import lombok.Data;
+import pl.strefainformacji.component.Category;
+import pl.strefainformacji.component.Tag;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 @Data
 public class ArticleRequest {
-    private Long articleId;
     private String title;
     private String shortDescription;
     private String description;
-    private Boolean isSubscriber;
+    private List<Category> categories;
+    private List<Tag> tags;
     private LocalDateTime createdAt;
 }
