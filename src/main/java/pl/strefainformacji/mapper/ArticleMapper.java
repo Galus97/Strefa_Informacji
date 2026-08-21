ad76d6583de13abac0edc71d7e3afb1ff7943ac4
michalgalus <galusmichal@outlook.com>
2026-08-21 05:37:14 +0200

    SCRUM-01 added toArticleResponse to ArticleMapper

--- src/main/java/pl/strefainformacji/mapper/ArticleMapper.java
+++ src/main/java/pl/strefainformacji/mapper/ArticleMapper.java
@@ -1,8 +1,22 @@
 package pl.strefainformacji.mapper;
 
 import org.springframework.stereotype.Component;
+import pl.strefainformacji.dto.response.ArticleResponse;
+import pl.strefainformacji.entity.Article;
+
+import java.util.List;
 
 @Component
 public class ArticleMapper {
+
+    public ArticleResponse toArticleResponse(Article article) {
+        return new ArticleResponse(article.getArticleId(),
+                article.getTitle(),
+                article.getShortDescription(),
+                article.getDescription(),
+                article.getCategories(),
+                article.getTags(),
+                article.getCreatedAt());
+    }
 
 }
