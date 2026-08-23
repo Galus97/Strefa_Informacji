6175ddf1e75f8104845e49fc4038c75618e6ada7
michalgalus <galusmichal@outlook.com>
2026-08-23 20:55:37 +0200

    SCRUM-01 added toImageResponse to ImageMapper

--- src/main/java/pl/strefainformacji/mapper/ImageMapper.java
+++ src/main/java/pl/strefainformacji/mapper/ImageMapper.java
@@ -1,8 +1,18 @@
 package pl.strefainformacji.mapper;
 
 import org.springframework.stereotype.Component;
+import pl.strefainformacji.dto.response.ImageResponse;
+import pl.strefainformacji.entity.Image;
 
 @Component
 public class ImageMapper {
 
+    public static ImageResponse toImageResponse(Image image) {
+        return new ImageResponse(
+                image.getImageId(),
+                image.getSrcImg(),
+                image.getAltImg(),
+                image.getArticle().getArticleId()
+        );
+    }
 }
