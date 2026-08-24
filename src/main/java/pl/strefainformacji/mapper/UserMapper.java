c0c2c8f761cfc0c95923fbe18663ec22788b76aa
michalgalus <galusmichal@outlook.com>
2026-08-24 11:46:05 +0200

    SCRUM-01 added toUserModel to UserMapper

--- src/main/java/pl/strefainformacji/mapper/UserMapper.java
+++ src/main/java/pl/strefainformacji/mapper/UserMapper.java
@@ -1,7 +1,30 @@
 package pl.strefainformacji.mapper;
 
 import org.springframework.stereotype.Component;
+import pl.strefainformacji.dto.request.UserRequest;
+import pl.strefainformacji.dto.response.UserResponse;
+import pl.strefainformacji.entity.User;
 
 @Component
 public class UserMapper {
+
+    public static UserResponse toUserResponse(User user) {
+        return new UserResponse(
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.isEnabled(),
+                user.isSubscriber()
+        );
+    }
+
+    public static User toUserModel(UserRequest userRequest) {
+        return User.builder()
+                .firstName(userRequest.getFirstName())
+                .lastName(userRequest.getLastName())
+                .email(userRequest.getEmail())
+                .password(userRequest.getPassword())
+                .isSubscriber(userRequest.isSubscriber())
+                .build();
+    }
 }
