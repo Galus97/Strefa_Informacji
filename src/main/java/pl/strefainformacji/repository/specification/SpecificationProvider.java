109fc5949efbb059d341ae00c426ade0eec4af35
michalgalus <galusmichal@outlook.com>
2026-08-25 22:24:01 +0200

    SCRUM-01 added methods to SpecificationProvider

--- src/main/java/pl/strefainformacji/repository/specification/SpecificationProvider.java
+++ src/main/java/pl/strefainformacji/repository/specification/SpecificationProvider.java
@@ -1,4 +1,8 @@
 package pl.strefainformacji.repository.specification;
 
-public interface SpecificationProvider {
+import org.springframework.data.jpa.domain.Specification;
+
+public interface SpecificationProvider<T> {
+    String getKey();
+    Specification<T> getSpecification(String[] params);
 }
