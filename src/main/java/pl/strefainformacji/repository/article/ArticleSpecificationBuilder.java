25a93e3d92471c94cb9992010d2121eddb767988
michalgalus <galusmichal@outlook.com>
2026-08-27 12:46:38 +0200

    SCRUM-01 added ArticleSpecificationBuilder

---
+++ src/main/java/pl/strefainformacji/repository/article/ArticleSpecificationBuilder.java
@@ -0,0 +1,10 @@
+package pl.strefainformacji.repository.article;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ArticleSpecificationBuilder {
+
+}
