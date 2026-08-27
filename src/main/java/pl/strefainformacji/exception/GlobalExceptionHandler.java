4a71b6277c29e481c27b4651ee6f4091b028b8c9
michalgalus <galusmichal@outlook.com>
2026-08-27 21:05:42 +0200

    SCRUM-01 added handleMethodArgumentNotValid to GlobalExceptionHandler

--- src/main/java/pl/strefainformacji/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/strefainformacji/exception/GlobalExceptionHandler.java
@@ -1,17 +1,27 @@
 package pl.strefainformacji.exception;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import pl.strefainformacji.component.ErrorMessages;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ArticleNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleArticleNotFoundException (ArticleNotFoundException e) {
@@ -42,5 +52,31 @@ public class GlobalExceptionHandler {
         Map<String, String> response = new HashMap<>();
         response.put(ErrorMessages.ERROR, e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST);
+        List<String> mappedErrors = ex.getBindingResult().getAllErrors().stream()
+                .map(this::getErrorMessage)
+                .toList();
+        body.put("errors", mappedErrors);
+        return new ResponseEntity<>(body, headers, status);
+    }
+
+    private String getErrorMessage(ObjectError objectError) {
+        if (objectError instanceof FieldError) {
+            String field = ((FieldError) objectError).getField();
+            String defaultMessage = objectError.getDefaultMessage();
+            return field + " " + defaultMessage;
+        }
+        return objectError.getDefaultMessage();
     }
 }
