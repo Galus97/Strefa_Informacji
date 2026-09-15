33606d648cb9c6008233adfc7b09e70ac658b36e
michalgalus <galusmichal@outlook.com>
2026-09-15 14:23:28 +0200

    SCRUM-01 added generateToken to JwtUtil

--- src/main/java/pl/strefainformacji/util/JwtUtil.java
+++ src/main/java/pl/strefainformacji/util/JwtUtil.java
@@ -1,7 +1,31 @@
 package pl.strefainformacji.util;
 
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
 
 @Component
 public class JwtUtil {
+    private Key secret;
+
+    public JwtUtil(@Value("${jwt.secret}") String secretString) {
+        secret = Keys.hmacShaKeyFor(secretString.getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(secret)
+                .compact();
+    }
 }
