dc15880d7b57fe15fdd9917fd607d53e4ea83c9c
michalgalus <galusmichal@outlook.com>
2026-09-15 14:29:22 +0200

    SCRUM-01 added getClaimFromToken to JwtUtil

--- src/main/java/pl/strefainformacji/util/JwtUtil.java
+++ src/main/java/pl/strefainformacji/util/JwtUtil.java
@@ -1,13 +1,17 @@
 package pl.strefainformacji.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.security.Keys;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
@@ -27,5 +31,24 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(secret)
                 .compact();
+    }
+
+    public boolean isValidToken(String token) {
+        Jws<Claims> claimsJws = Jwts.parser()
+                .verifyWith((SecretKey) secret)
+                .build()
+                .parseSignedClaims(token);
+
+        return claimsJws.getPayload().getExpiration().before(new Date());
+    }
+
+
+    private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = Jwts.parser()
+                .verifyWith((SecretKey) secret)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return claimsResolver.apply(claims);
     }
 }
